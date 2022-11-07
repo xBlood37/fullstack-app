8d608e7f422f3f82909f1de0b139037fd2d6f36e
xBlood37 <123d.123b@mail.ru>
2022-11-07 16:01:57 +0300

    backend finish

--- routes/order.js
+++ routes/order.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const controller = require("../controllers/order");
 const router = express.Router();
+const passport = require("passport");
 
 // localhost:5000/api/order/getAll
-router.get("/", controller.getAll);
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  controller.getAll
+);
 
 // localhost:5000/api/order/create
-router.post("/", controller.create);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  controller.create
+);
 
 module.exports = router;
--- routes/position.js
+++ routes/position.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const conroller = require("../controllers/position");
 const router = express.Router();
+const passport = require("passport");
 
 // localhost:5000/api/position/getByCategoryId
-router.get("/:categoryId", conroller.getByCategoryId);
+router.get(
+  "/:categoryId",
+  passport.authenticate("jwt", { session: false }),
+  conroller.getByCategoryId
+);
 
 // localhost:5000/api/position/create
-router.post("/", conroller.create);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  conroller.create
+);
 
 // localhost:5000/api/position/update
-router.patch("/:id", conroller.update);
+router.patch(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  conroller.update
+);
 
 // localhost:5000/api/position/remove
-router.delete("/:id", conroller.remove);
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  conroller.remove
+);
 
 module.exports = router;
